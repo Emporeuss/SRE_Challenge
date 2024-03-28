61dff36c09d3495bdee5f02fe128abaf808685df
Emporeuss <vova.futureen@gmail.com>
2024-03-28 06:47:21 +0200

    Modified task

--- task-3/speed.sh
+++ task-3/speed.sh
@@ -7,9 +7,10 @@ threshold=0.1
 download_time=$(curl -s -w "%{time_total}\n" -o /dev/null https://google.com)
 
 
-## if-fi statemen tthat will compare two values
+## if-fi statement that will compare two values
 if (( $(bc <<< "$download_time > $threshold") )); then
 
 # curl command that will send POST request to my Slack webhook in case threshold is exceeded
-curl --location -X POST 'https://hooks.slack.com/services/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' --header 'Content-Type: application/json' --data-raw '{"text": "Achtung! Website load time exceeds threshold!" }'
+curl --location -X POST 'https://hooks.slack.com/services/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' --header 'Content-Type: application/json' --data-raw "{\"text\": \"Achtung! Website loads rather long: $download_time\"}"
+
 fi
